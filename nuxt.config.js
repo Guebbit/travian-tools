d30223ba73349c41b1040bde48048b6fc16ced95
guebbit <guerzoni.andrea91@gmail.com>
2021-07-25 15:06:59 +0200

    typescript fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,8 +98,9 @@ export default {
         }
     },
 
+
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
-        //extractCSS: true,
+        extractCSS: true,
     }
 }
