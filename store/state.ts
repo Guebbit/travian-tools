77e1bf4365df086ae1fad2426f17c20c83bd6be0
guebbit <guerzoni.andrea91@gmail.com>
2021-07-25 15:49:07 +0200

    map size back to 400?

--- store/state.ts
+++ store/state.ts
@@ -12,7 +12,7 @@ export default () :stateMap => {
 		troopDetails: {},
 		serverData: {
 			serverName: 'ts2.travian.it',
-			mapSize: 201,	//+1?
+			mapSize: 401,	//+1?
 			speed: 1,
 			arenaCap: 20,
 		},
